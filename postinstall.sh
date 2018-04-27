e4317d9f3987f91299bcb397c257d3ed713f13eb
Auston Stewart <auston@shedbuilt.net>
2018-04-27 08:08:54 -1000

    Fixing post-install issues

--- postinstall.sh
+++ postinstall.sh
@@ -6,7 +6,7 @@ fi
 # In Amano, add resolve to nsswitch.conf if unmodified
 SHED_PKG_NSSWITCH_HOSTS="$(sed -n 's/^hosts: //p' /etc/nsswitch.conf)"
 if [ "$SHED_PKG_NSSWITCH_HOSTS" == 'files dns' ]; then
-    sed -i "s/hosts: .*/hosts: files [NOTFOUND=return] resolve [!UNAVAIL=return] dns/g" /etc/nsswitch.conf
+    sed -i "s/hosts: .*/hosts: files mymachines resolve [!UNAVAIL=return] dns myhostname/g" /etc/nsswitch.conf
 fi
 # Bring down systemd-networkd interfaces
 systemctl is-active systemd-networkd > /dev/null 2>&1
@@ -18,7 +18,7 @@ if [ $? -eq 0 ]; then
 fi
 # Bring up NetworkManager
 systemctl is-active NetworkManager > /dev/null 2>&1
-if [ $? -eq 0]; then
+if [ $? -eq 0 ]; then
     systemctl restart NetworkManager
 else
     systemctl enable NetworkManager
